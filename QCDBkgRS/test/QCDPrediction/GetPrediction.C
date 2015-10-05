71e33d1097a10b51c50b26bd12f46f1b60ac3a03
Jory Sonneveld <jory.sonneveld@cern.ch>
2015-10-05 22:48:46 +0200

    First gensmear test

--- QCDBkgRS/test/QCDPrediction/GetPrediction.C
+++ QCDBkgRS/test/QCDPrediction/GetPrediction.C
@@ -45,8 +45,8 @@ TCanvas* DrawComparison(TH1F* prediction, TH1F* selection, TString Title, TStrin
       titlePrediction = "Data-driven Pred. from MC";
       //titlePrediction = "Smeared Generator Jets";
       titleSelection = "MC Expectation";
-      RatioTitle = "(Pred-MC)/MC";
-      //RatioTitle = "(Gen-MC)/MC";
+      //RatioTitle = "(Pred-MC)/MC";
+      RatioTitle = "(Gen-MC)/MC";
    }
    
    static Int_t c_LightBrown   = TColor::GetColor( "#D9D9CC" );
@@ -219,8 +219,8 @@ int main()
    TChain* prediction = new TChain("QCDfromSmearing/QCDPrediction");
    TChain* selection = new TChain("RA2TreeMaker/PreSelection");
    
-   ifstream myfile1 ("filelists_phys14/filelist.txt");
-   //ifstream myfile1 ("filelists_phys14/test.txt");
+   //ifstream myfile1 ("filelists_phys14/filelist.txt");
+   ifstream myfile1 ("filelists_phys14/test.txt");
 
    if (myfile1.is_open()) {
       while( myfile1.good() ) {
@@ -250,10 +250,10 @@ int main()
    else LumiTitle = "CMS Simulation, L = 10 fb^{  -1}, #sqrt{s} = 13 TeV";
    //else LumiTitle = "CMS work in progress, #sqrt{s} = 13 TeV";
    
-   TString postfix = "_MGMLM_bestMatching_withCleverRBcorr_pt10_angResNew_withNeutrinos";
+   //TString postfix = "_MGMLM_bestMatching_withCleverRBcorr_pt10_angResNew_withNeutrinos";
    //TString postfix = "_MGMLM_bestMatching_withoutRBcorr_pt10_angRes";
    //TString postfix = "_MGMLM_GenSmear_fineBins_bestMatching_algo_noSeedCut";
-   //TString postfix = "_test";
+   TString postfix = "_test";
    
    vector<TString> xTitle_presel;
    xTitle_presel.push_back("H_{T} (GeV)");
