e001a8ef49e5fe52d9f097adc95f1fa80de235d3
Jory Sonneveld <jory.sonneveld@desy.de>
2015-12-23 14:50:32 +0100

    Checking impact of clever rebalance corrections
    Clever rebalance corrections means not just the mean of the rebalance
    corrections for b and non-b jets is used, but actually with a certain
    probability the pt-dependent rebalance correction is used.

--- QCDBkgRS/test/QCDPrediction/GetPrediction.C
+++ QCDBkgRS/test/QCDPrediction/GetPrediction.C
@@ -250,7 +250,7 @@ int main()
    else LumiTitle = "CMS Simulation, L = 10 fb^{  -1}, #sqrt{s} = 13 TeV";
    //else LumiTitle = "CMS work in progress, #sqrt{s} = 13 TeV";
    
-   TString postfix = "_MGMLM_RandS_bestMatching_angResNew_withNeutrinos_Everywhere_MHTall";
+   TString postfix = "_MGMLM_RandS_bestMatching_angResNew_withNeutrinos_Everywhere_RecoPTBins_DeadECALTP";
    //TString postfix = "_MGMLM_GenSmear_bestMatching_angResNew_withNeutrinos_Christian_noBTagCorrections";
    //TString postfix = "_MGMLM_bestMatching_withoutRBcorr_pt10_angRes";
    //TString postfix = "_MGMLM_GenSmear_fineBins_bestMatching_algo_noSeedCut";
